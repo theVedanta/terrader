17407e2c36c69229b2e2a6191bab487c9e32302b
Vedanta Somnathe <vedanta1412@gmail.com>
2023-04-30 03:36:42 +0530

    designers

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,23 @@
+import { Button, Flex } from "@chakra-ui/react";
+import Link from "next/link";
+
 const Home = () => {
-    return <></>;
+    return (
+        <>
+            <Flex
+                sx={{
+                    w: "100vw",
+                    h: "100vh",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <Link href="/dash">
+                    <Button size="lg">Dashboard</Button>
+                </Link>
+            </Flex>
+        </>
+    );
 };
 
 export default Home;

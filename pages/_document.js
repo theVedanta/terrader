c9d8a7cae7974fe6119b0613b5e9ec670584184a
Vedanta Somnathe <vedanta1412@gmail.com>
2023-04-30 03:28:00 +0530

    bg

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                <title>Terrader</title>
+            </Head>
             <body>
                 <ColorModeScript
                     initialColorMode={theme.config.initialColorMode}
